2ba678d95f39732a2b8fe481176172b54be6a3b0
pmbusch <pmbuschh@gmail.com>
2024-11-22 16:26:07 -0800

    Add scripts GHG

--- Scripts/GHG Model/Fleet_Model.R
+++ Scripts/GHG Model/Fleet_Model.R
@@ -7,7 +7,6 @@
 source("Scripts/00-Libraries.R", encoding = "UTF-8")
 
 
-
 ## Reuse statistics from Survival Model -----
 reuse <- read.csv("Parameters/Demand Intermediate Results/region_outflows_LIB.csv",
                   stringsAsFactors = FALSE)
@@ -23,12 +22,14 @@ df <- reuse %>% filter(Region=="United States")
 
 # Fleet at 2050
 (x <- df %>% filter(Powertrain=="BEV",Vehicle=="Car",Year==2050) %>% 
-    pull(EV_Stock)/1e6))
+    pull(EV_Stock)/1e6)
 x/335 # car ownership 0.70
 
 
-
-df <- df %>% dplyr::select(Vehicle,Powertrain,Year,Scenario,Sales,EV_Stock)
+# Select only relevant columns
+# add recycling flows later
+df <- df %>% dplyr::select(Vehicle,Powertrain,Year,Scenario,Sales,EV_Stock,
+                           add_LIB)
 
 head(df)
 
@@ -38,6 +39,7 @@ write.csv(df,"Results/GHG/USA_fleet.csv",row.names = F)
 
 # Figures ------
 names(df)
+url_fig <- "Figures/GHG/%s.png"
 
 # Sales
 df %>% 
@@ -51,6 +53,9 @@ df %>%
   labs(x="",y="",title="USA BEV Sales [million units]")+
   theme(legend.position = "none")
 
+ggsave(sprintf(url_fig,"sales"),dpi=600,units = "cm",
+       width = 12,height=8.7)
+
 # Fleet
 df %>% 
   filter(Year<2051) %>% 
@@ -62,6 +67,29 @@ df %>%
   scale_x_continuous(breaks = c(2022, 2030, 2040, 2050))+
   labs(x="",y="",title="USA BEV Fleet [million units]")+
   theme(legend.position = "none")
+
+ggsave(sprintf(url_fig,"fleet"),dpi=600,units = "cm",
+       width = 12,height=8.7)
+
+
+
+# Battery
+df %>% 
+  filter(Year<2051) %>% 
+  filter(Powertrain=="BEV") %>% 
+  mutate(`Replacement LIB`=add_LIB/1e6) %>% 
+  mutate(`EV Sales`=Sales/1e6) %>% 
+  dplyr::select(Year,`Replacement LIB`,`EV Sales`,Vehicle) %>% 
+  pivot_longer(c(-Year,-Vehicle), names_to = "key", values_to = "value") %>% 
+  ggplot(aes(Year,value,fill=key))+
+  geom_area()+
+  facet_wrap(~Vehicle,scales = "free_y")+
+  scale_x_continuous(breaks = c(2022, 2030, 2040, 2050))+
+  labs(x="",y="",fill="",title="USA LIB Need [million units]")+
+  theme(legend.position = "bottom")
+
+ggsave(sprintf(url_fig,"LIB"),dpi=600,units = "cm",
+       width = 12,height=8.7)
 
 
 # EoF
